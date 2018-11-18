e05cfe5090b8ad1850ff8297be5259e10f673162
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-11-18 19:39:25 +0700

    Create humaditySensor.ino

---
+++ iotTeam/humaditySensor.ino
@@ -0,0 +1,30 @@
+/*  DHT11/ DHT22 Sensor Temperature and Humidity Tutorial
+ *  Program made by Dejan Nedelkovski,
+ *  www.HowToMechatronics.com 
+ */
+/*
+ * You can find the DHT Library from Arduino official website
+ * https://playground.arduino.cc/Main/DHTLib
+ */
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+#include <dht.h>
+#define dataPin 8
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+dht DHT;
+void setup() {
+  lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+}
+void loop() {
+  int readData = DHT.read22(dataPin);
+  float t = DHT.temperature;
+  float h = DHT.humidity;
+  lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Temp.: "); // Prints string "Temp." on the LCD
+  lcd.print(t); // Prints the temperature value from the sensor
+  lcd.print(" C");
+  lcd.setCursor(0,1);
+  lcd.print("Humi.: ");
+  lcd.print(h);
+  lcd.print(" %");
+  delay(2000);
+}
